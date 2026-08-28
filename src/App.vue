1f70402a58b59ec549f67604b24861c4693ed0b7
Ryuko98 <daniel123hernandez15@gmail.com>
2026-08-27 22:01:20 -0600

    Footer de la página

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,12 @@
   <MenuNav />
   <!-- Se llama al componente Router donde se carga la vista correspondiente y no se recargue la página  -->
   <RouterView />
+
+  <Footer />
 </template>
 
 <script setup>
   import MenuNav from './componentes/MenuNav.vue';
+  import Footer from './componentes/Footer.vue';
   import { RouterView } from 'vue-router'; 
 </script>
