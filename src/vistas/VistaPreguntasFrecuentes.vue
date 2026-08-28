9e97f9eede226d4424870be59c9db37035e437d0
Jonathan Marchelli <93404562+Uki-182@users.noreply.github.com>
2026-08-27 22:25:53 -0600

    Preguntas Frecuentes

--- src/vistas/VistaPreguntasFrecuentes.vue
+++ src/vistas/VistaPreguntasFrecuentes.vue
@@ -1,6 +1,26 @@
 <template>
-    <h1 class="text-2xl text-center pt-10 ">PREGUNTAS FRECUENTES</h1>
+    <div class="my-20">
+        <h1 class="text-4xl text-center text-brand-500 font-bold">Preguntas Frecuentes</h1>
+        <div class="mt-14 flex flex-col items-center">
+            <ListaPreguntaFrecuente
+                titulo="Preguntas Generales"
+            />
+            <ListaPreguntaFrecuente
+                titulo="Pedidos y Compras"
+            />
+            <ListaPreguntaFrecuente
+                titulo="Envíos y Entregas"
+            />
+            <ListaPreguntaFrecuente
+                titulo="Devoluciones y garantías"
+            />
+            <ListaPreguntaFrecuente
+                titulo="Productos e Inventario"
+            />
+        </div>
+    </div>
 </template>
 
 <script setup>
+    import ListaPreguntaFrecuente from '@/componentes/ListaPreguntaFrecuente.vue';
 </script>
