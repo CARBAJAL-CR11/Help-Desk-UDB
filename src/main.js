adcd7eaa30fd3849361598b4ec033c00e33b489a
Ryuko98 <daniel123hernandez15@gmail.com>
2026-08-27 21:31:36 -0600

    método para importar iconos desde TablerIcons

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import router from './router'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as TablerIcons from '@tabler/icons-vue';
+
+
 
 const app = createApp(App)
+
+
+
+for (const [key, component] of Object.entries(TablerIcons)) {
+  app.component(key, component);
+}
 
 app.use(router);
 
