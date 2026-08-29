bf37b7b892cdb78edaf22cad50db3c137763f624
Jonathan Marchelli <93404562+Uki-182@users.noreply.github.com>
2026-08-28 22:57:51 -0600

    Acordion funcional
    Ya hay una forma de mostrar el acordion de las preguntas, falta aplicar el diseño

--- src/vistas/VistaPreguntasFrecuentes.vue
+++ src/vistas/VistaPreguntasFrecuentes.vue
@@ -4,18 +4,32 @@
         <div class="mt-14 flex flex-col items-center">
             <ListaPreguntaFrecuente
                 titulo="Preguntas Generales"
-            />
+                v-model="acordionAbierto"                
+            >
+                <ListaPreguntaFrecuente
+                    titulo="¿Cuáles son sus horarios de atencion?"
+                    tipo="hijo"
+                />
+                <ListaPreguntaFrecuente
+                    titulo="¿Dónde están ubicados y cómo puedo llegar?"
+                    tipo="hijo"
+                />
+            </ListaPreguntaFrecuente>
             <ListaPreguntaFrecuente
                 titulo="Pedidos y Compras"
+                v-model="acordionAbierto"
             />
             <ListaPreguntaFrecuente
                 titulo="Envíos y Entregas"
+                v-model="acordionAbierto"
             />
             <ListaPreguntaFrecuente
                 titulo="Devoluciones y garantías"
+                v-model="acordionAbierto"
             />
             <ListaPreguntaFrecuente
                 titulo="Productos e Inventario"
+                v-model="acordionAbierto"
             />
         </div>
     </div>
@@ -23,4 +37,7 @@
 
 <script setup>
     import ListaPreguntaFrecuente from '@/componentes/ListaPreguntaFrecuente.vue';
+    import { ref } from 'vue';
+
+    const acordionAbierto = ref('');
 </script>
